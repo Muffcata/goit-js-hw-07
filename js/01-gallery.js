691b9652133e5146c9fcbbddb304bd5394ecc609
Muffcata <marta.oched@gmail.com>
2022-12-15 23:56:26 +0100

    whatever

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const { log } = console;
+const photos = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createImagesGallery = () => {
+  galleryItems.forEach((item) => {
+    const box = document.createElement("div");
+    box.classList.add("gallery__item");
+    photos.append(box);
+    const innerBox = document.createElement("a");
+    innerBox.classList.add("gallery__link");
+
+    box.appendChild(innerBox);
+    const imageBox = document.createElement("img");
+    imageBox.textContent = item;
+    imageBox.classList.add("gallery__image");
+    innerBox.appendChild(imageBox);
+  });
+};
+
+log(photos);
