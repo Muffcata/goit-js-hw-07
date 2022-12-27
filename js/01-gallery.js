90dff895d75ded7c43e47e501a02e0dbe02e7222
Muffcata <marta.oched@gmail.com>
2022-12-27 21:22:53 +0100

    change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,20 +3,16 @@ import { galleryItems } from "./gallery-items.js";
 const { log } = console;
 const photos = document.querySelector(".gallery");
 
-const createImagesGallery = () => {
-  galleryItems.forEach((item) => {
-    const box = document.createElement("div");
-    box.classList.add("gallery__item");
-    photos.append(box);
-    const innerBox = document.createElement("a");
-    innerBox.classList.add("gallery__link");
+galleryItems.forEach((item) => {
+  const box = document.createElement("div");
+  box.classList.add("gallery__item");
+  photos.append(box);
+  const innerBox = document.createElement("a");
+  innerBox.classList.add("gallery__link");
 
-    box.appendChild(innerBox);
-    const imageBox = document.createElement("img");
-    imageBox.textContent = item;
-    imageBox.classList.add("gallery__image");
-    innerBox.appendChild(imageBox);
-  });
-};
-
-log(photos);
+  box.append(innerBox);
+  const imageBox = document.createElement("img");
+  imageBox.textContent = item;
+  imageBox.classList.add("gallery__image");
+  innerBox.append(imageBox);
+});
