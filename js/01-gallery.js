74eb1aa8fbece4aed671363aac2011b1ce59c70f
Muffcata <marta.oched@gmail.com>
2023-01-01 16:45:45 +0100

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const { log } = console;
 const photos = document.querySelector(".gallery");
 
@@ -9,10 +10,27 @@ galleryItems.forEach((item) => {
   photos.append(box);
   const innerBox = document.createElement("a");
   innerBox.classList.add("gallery__link");
+  innerBox.href = item.original;
 
   box.append(innerBox);
   const imageBox = document.createElement("img");
   imageBox.textContent = item;
+  imageBox.src = item.preview;
+  imageBox.alt = item.description;
+  imageBox.dataset.source = item.original;
   imageBox.classList.add("gallery__image");
   innerBox.append(imageBox);
 });
+
+const choosePhotos = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src=${e.target.dataset.source} />`
+  );
+  instance.show();
+};
+
+photos.addEventListener("click", choosePhotos);
